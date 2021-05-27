70ec6a425628cc8403a1c433eea0029cd762d918
joyfullservice <joyfullservice@users.noreply.github.com>
2021-05-27 16:12:01 -0500

    Rename conflicts table

--- Version Control.accda.src/tbldefs/tblConflicts.sql
+++ Version Control.accda.src/tbldefs/tblConflicts.sql
@@ -1,8 +1,9 @@
-﻿CREATE TABLE [tblConflict] (
+﻿CREATE TABLE [tblConflicts] (
   [ID] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
   [Component] VARCHAR (255),
   [FileName] VARCHAR (255),
   [ObjectDate] DATETIME ,
+  [IndexDate] DATETIME ,
   [FileDate] DATETIME ,
   [Resolution] LONG ,
   [Diff] LONGTEXT 
