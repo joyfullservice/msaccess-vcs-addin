5dedb47d13435921f9e06487335f1d51af67dca2
joyfullservice <joyfullservice@users.noreply.github.com>
2021-05-27 14:33:15 -0500

    Remove incomplete feature branch objects
    These objects are being implemented in specific feature branches which will be rolled into `dev` when they are completed. See discussion in #230

--- Version Control.accda.src/tbldefs/tblConflict.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [tblConflict] (
-  [ID] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
-  [Component] VARCHAR (255),
-  [FileName] VARCHAR (255),
-  [ObjectDate] DATETIME ,
-  [FileDate] DATETIME ,
-  [Resolution] LONG ,
-  [Diff] LONGTEXT 
-)
